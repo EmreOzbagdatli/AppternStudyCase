b7bd65c0be59df3ad30c0004986a2ad437d48841
Emre Özbağdatlı <74432853+EmreOzbagdatli@users.noreply.github.com>
2023-05-12 21:13:19 +0300

    Created
    project created.

---
+++ AppcentStudyCase/AppcentStudyCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppcentStudyCase
+//
+//  Created by Emre Özbağdatlı on 12.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
